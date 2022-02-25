4dffffb4ce94128b3ec458477af8bf4963dfbcc6
denis <permwed@gmail.com>
2022-02-25 21:43:40 +0500

    redux cart work

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BiCartAlt } from "react-icons/bi";
+import { useSelector } from "react-redux";
 
 import "./cart-block.css";
 
 const CartBlock = () => {
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const totalPrice = items.reduce((acc, item) => (acc += item.price), 0);
+
   return (
     <div className="cart-block">
       <BiCartAlt size={25} className="cart-block__icon" />
-      <span className="cart-block__total-price">2133 руб.</span>
+      <span className="cart-block__total-price">{totalPrice} руб.</span>
     </div>
   );
 };
--- src/components/game-buy/game-buy.js
+++ src/components/game-buy/game-buy.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setItemInCart } from "../../redux/cart/reducer";
 
 // Components
 import Button from "../button/button";
@@ -6,10 +8,17 @@ import Button from "../button/button";
 import "./game-buy.css";
 
 const GameBuy = ({ game }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    e.stopPropagation(e);
+    dispatch(setItemInCart(game));
+  };
+
   return (
     <div className="game-buy">
       <span className="game-buy__price">{game.price}</span>
-      <Button onClick={() => null} type="primary">
+      <Button onClick={handleClick} type="primary">
         В корзину
       </Button>
     </div>
